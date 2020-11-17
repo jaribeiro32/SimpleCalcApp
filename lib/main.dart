7dd5ae365d0d61693a3ba14ea8544c5754a1e1b3
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-11-17 15:45:49 -0300

    simple math integer only

--- lib/main.dart
+++ lib/main.dart
@@ -10,36 +10,55 @@ class SimpleCalcApp extends StatefulWidget {
 }
 
 class _SimpleCalcAppState extends State<SimpleCalcApp> {
+  String result = '';
+  double number1 = 0.0;
+  double number2 = 0.0;
   @override
   Widget build(BuildContext context) {
     TextField num1 = TextField(
-        keyboardType: TextInputType.number,
-        style: TextStyle(color: Colors.deepOrange),
-        decoration: InputDecoration(
-            labelText: '1º Valor',
-            labelStyle: TextStyle(color: Colors.deepOrange),
-            enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.deepOrange, width: 1.0)),
-            focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.deepOrange, width: 1.0))));
+      keyboardType: TextInputType.number,
+      style: TextStyle(color: Colors.deepOrange),
+      decoration: InputDecoration(
+          labelText: '1º Valor',
+          labelStyle: TextStyle(color: Colors.deepOrange),
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.deepOrange, width: 1.0)),
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.deepOrange, width: 1.0))),
+      onChanged: (value) {
+        number1 = double.parse(value);
+      },
+    );
 
     TextField num2 = TextField(
-        keyboardType: TextInputType.number,
-        style: TextStyle(color: Colors.deepOrange),
-        decoration: InputDecoration(
-            labelText: '2º Valor',
-            labelStyle: TextStyle(color: Colors.deepOrange),
-            enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.deepOrange, width: 1.0)),
-            focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.deepOrange, width: 1.0))));
+      keyboardType: TextInputType.number,
+      style: TextStyle(color: Colors.deepOrange),
+      decoration: InputDecoration(
+          labelText: '2º Valor',
+          labelStyle: TextStyle(color: Colors.deepOrange),
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.deepOrange, width: 1.0)),
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.deepOrange, width: 1.0))),
+      onChanged: (value) => {number2 = double.parse(value)},
+    );
 
     RaisedButton button = RaisedButton(
-      onPressed: () {},
       child: Text('Calcular'),
       color: Colors.deepOrange,
       textColor: Colors.white,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.9)),
+      onPressed: () {
+        setState(() {
+          double calc = number1 + number2;
+          this.result = "O resultado da soma é ${calc.toStringAsFixed(0)}";
+        });
+      },
+    );
+
+    Text result = Text(
+      this.result,
+      style: TextStyle(color: Colors.deepOrange),
     );
 
     Padding separator = Padding(padding: EdgeInsets.all(4.0));
@@ -53,7 +72,9 @@ class _SimpleCalcAppState extends State<SimpleCalcApp> {
         SizedBox(
           child: button,
           width: double.infinity,
-        )
+        ),
+        separator,
+        result
       ],
     );
     return MaterialApp(
